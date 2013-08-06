ffd0e82ccc6a7f4f515a6d0c75c091403969b03c
mik356ua <mik356ua@gmail.com>
2013-08-06 23:01:08 +0300

    Now using full paths for -p argument

--- addvhost.sh
+++ addvhost.sh
@@ -11,7 +11,7 @@ elif [ $1 = "--help" ]; then
 Script adds new virtual host on local machine. Requires sudo permissions. Usage:
 
     sudo ./addvhost -n vhost_name [-u user_name]
-                   [-d vhost_public_dir] [-p project_dir]
+                   [-d vhost_public_dir] [-p projects_dir]
 
 Examples:
 
@@ -19,9 +19,9 @@ Examples:
 
 Creates vhost "mysite.root.local" in /var/www/mysite
 
-    sudo ./addvhost -n mysite -u webdev -d public -p projects/php
+    sudo ./addvhost -n mysite -u webuser -d public -p /home/webuser/sites
 
-This will create new vhost "mysite.user.local" in /home/webdev/projects/php/mysite with project root directory in /home/webdev/projects/php/mysite/public
+This will create new vhost "mysite.user.local" in /home/webuser/projects/php/mysite with project root directory in /home/webuser/projects/php/mysite/public
 
 EOL
     exit
@@ -31,8 +31,11 @@ fi
 VHOST_ROOT="/etc/apache2/sites-available"
 VHOST_NAME=
 VHOST_DIR=
+VHOST_URL=
 USER_NAME=$(whoami)
-PROJECT_DIR=
+PROJECT_DIR="/var/www"
+PROJECT_PATH=
+PROJECT_ROOT=
 
 # handle arguments
 for (( i = 1, j = 2; i < $#; i++, j++ )); do
@@ -47,7 +50,6 @@ for (( i = 1, j = 2; i < $#; i++, j++ )); do
             ;;
         -u)
             USER_NAME="$VAL"
-            USER_HOME=$(getent passwd $USER_NAME | cut -d: -f6)
             ;;
         -p)
             PROJECT_DIR="$VAL"
@@ -56,16 +58,20 @@ for (( i = 1, j = 2; i < $#; i++, j++ )); do
 done
 
 # define project path and vhost url
-if [[ -z $PROJECT_DIR ]] || [[ "$USER_NAME" == "root" ]]; then
-    PROJECT_PATH="/var/www/$VHOST_NAME"
-else
-    USER_HOME=$(getent passwd $USER_NAME | cut -d: -f6)
-    PROJECT_PATH="$USER_HOME/$PROJECT_DIR/$VHOST_NAME"
+if [[ ! -z "$VHOST_NAME" ]]; then
+    VHOST_URL="$VHOST_NAME.$USER_NAME.local"
 fi
-VHOST_URL="$VHOST_NAME.$USER_NAME.local"
+if [[ ! -z "$PROJECT_DIR" ]]; then
+    PROJECT_PATH="$PROJECT_DIR/$VHOST_NAME"
+fi
+if [[ ! -z "$VHOST_DIR" ]]; then
+    PROJECT_ROOT="$PROJECT_PATH/$VHOST_DIR"
+else
+    PROJECT_ROOT="$PROJECT_PATH"
+fi
 
 # check required vars
-if [[ -z "$VHOST_NAME" ]]; then
+if [[ -z "$VHOST_URL" ]]; then
     echo "Host name not specified. Run ./addvhost --help for help."
     exit
 fi
@@ -83,8 +89,8 @@ cat > "$VHOST_ROOT/$VHOST_NAME" <<EOL
     ServerAdmin $USER_NAME@$VHOST_URL
     ServerName $VHOST_URL
 
-    DocumentRoot $PROJECT_PATH/$VHOST_DIR
-    <Directory $PROJECT_PATH/$VHOST_DIR/>
+    DocumentRoot $PROJECT_ROOT
+    <Directory $PROJECT_ROOT/>
         Options Indexes FollowSymLinks MultiViews
         AllowOverride All
         Order allow,deny
